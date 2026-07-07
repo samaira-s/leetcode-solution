9c603b31de8834a9cf3a1b827ce1f23f726afb80
samaira-s <iamsamaira.singh@gmail.com>
2026-07-07 14:27:59 +0530

    Time: 2 ms (47.59%), Space: 48.9 MB (52.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,24 +1,41 @@
-import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     int n= nums2.length;
-     int m =nums1.length;
-     int k=m+n;
-     double[] s =new double[m+n];
-     for(int i=0;i<n;i++){
-        s[i]=nums2[i];
-     }   
-    for(int i=0;i<m;i++){
-        s[n+i]=nums1[i];
-    }
-    Arrays.sort(s);
-     double p=0;
-    if(k%2==0){
-         p=(s[k/2-1]+s[k/2])/2;
-    }
-    else{
-        p=s[(k+1)/2-1];
-    }
-    return p;
+        if (nums1.length == 0 && nums2.length == 0) return 0;
+        int[] merged = new int[nums1.length + nums2.length];
+        
+        int ctr1 = 0;
+        int ctr2 = 0;
+        int ctr3 = 0;
+
+        while (ctr1 < nums1.length && ctr2 < nums2.length) {
+            if (nums1[ctr1] <= nums2[ctr2]) {
+                merged[ctr3] = nums1[ctr1];
+                ctr1++;
+            } else {
+                merged[ctr3] = nums2[ctr2];
+                ctr2++;
+            }
+            ctr3++;
+            //System.out.println("loop1 " + ctr3 + " " + ctr1 + " " + ctr2);
+        }
+        while (ctr1 < nums1.length) {
+            merged[ctr3] = nums1[ctr1];
+            ctr1++;
+            ctr3++;
+            //System.out.println("loop2 " + ctr3 + " " + ctr1 + " " + ctr2);
+        }
+        while (ctr2 < nums2.length) {
+            merged[ctr3] = nums2[ctr2];
+            ctr2++;
+            ctr3++;
+            //System.out.println("loop3 " + ctr3 + " " + ctr1 + " " + ctr2);
+        }
+
+        if (merged.length % 2 == 0) {
+            int upperMedian = merged.length / 2;
+            return ((double) merged[upperMedian] + (double)(merged[upperMedian - 1])) / 2;
+        } else {
+            return merged[merged.length / 2];
+        }
     }
 }
