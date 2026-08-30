8db568f34fddc23dcd3b9fa964a840d5778d3387
samaira-s <iamsamaira.singh@gmail.com>
2026-08-30 23:48:10 +0530

    Time: 3 ms (98.21%), Space: 77 MB (95.96%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+    int n=height.length;
+    int l=0;
+    int r=n-1;
+    int max=0;
+   while(l<r){
+    int temo=0;
+if(height[l]<height[r]){
+temo=height[l]*(r-l);
+l++;}
+else{
+   temo=height[r]*(r-l);
+   r--; 
+}
+if(max<temo){
+    max=temo;
+}
+
+
+    }
+    return max;
+    }
+}
