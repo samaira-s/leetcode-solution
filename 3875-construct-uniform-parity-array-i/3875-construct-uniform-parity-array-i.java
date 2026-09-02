769b2e4012ae7c56abc8802e19df6df739233007
samaira-s <iamsamaira.singh@gmail.com>
2026-09-02 21:21:15 +0530

    Time: 2 ms (2.36%), Space: 45.3 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean uniformArray(int[] nums1) {
         Stack<Integer> even=new Stack<>();
         Stack<Integer> odd=new Stack<>();
-        int[] arr=new int[nums1.length];
+   
         for(int k:nums1){
             if(k%2==0){
             even.push(k);}
